02429cff695557fe9d9f87eefb6f38737249e00d
Augusto <augusto170599@gmail.com>
2025-12-14 14:17:50 -0300

    isset function added

---
+++ site/site.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $characterName = "Augusto";
+        $characterAge = 26;
+        echo("Hello World, my name is $characterName and my age is $characterAge");
+    ?>
+
+    <form action="site.php" method="get">
+        Name: <input type="text" name="name">
+    
+        Age: <input type="text" name="age">
+        <input type="submit">
+    </form>
+    Your name is: 
+    <?php 
+        if(isset($_GET["name"])) {
+            echo  $_GET["name"];
+        }
+        
+    ?>
+    Your age is: 
+    <?php 
+        if(isset($_GET["age"])) {
+            echo  $_GET["age"];
+        }
+        
+    ?>
+
+</body>
+</html>
