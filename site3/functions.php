a769440b299bcd33f2d17e06d22a60f84668a59e
Augusto <augusto170599@gmail.com>
2025-12-16 15:04:47 -0300

    function with if statement evaluates if number is prime or not and returns square or cube

--- site3/functions.php
+++ site3/functions.php
@@ -41,5 +41,31 @@
         }
     ?>
 
+    <hr>
+    Function with if statement<br>
+     <form action="functions.php" method="get">
+        <input type="text" name="number2">
+        <input type="submit">
+    </form>
+
+    <?php
+    
+        function square($num) {
+            return $num*$num;
+        }
+
+        if (isset($_GET["number2"])) {
+        $number= $_GET["number2"];
+        
+            if($number % 2 == 0 || $number % 3 == 0) {
+                echo("The number is not a prime number <br>");
+                echo square($number);
+            } else {
+                echo("The number is prime <br>");
+                echo cube($number);
+            }
+        }
+    ?>
+
 </body>
 </html>
