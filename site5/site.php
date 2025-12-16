7c723447a9bc98e31c654a5949fdf4b3b0bbe090
Augusto <augusto170599@gmail.com>
2025-12-16 17:02:19 -0300

    include a php file

---
+++ site5/site.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $title = "First Post";
+        $author = "Augusto";
+        $wordCount = 400;
+        include "article-header.php";
+        include "useful-tools.php";
+        echo("<br>");
+        sayHi("Augusto");
+    ?>
+
+</body>
+</html>
