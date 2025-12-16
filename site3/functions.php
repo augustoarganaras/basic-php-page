fbe4377050b9f9f337c3dc1506f1a2e417daef08
Augusto <augusto170599@gmail.com>
2025-12-16 14:56:08 -0300

    function with return statement

--- site3/functions.php
+++ site3/functions.php
@@ -6,22 +6,39 @@
     <title>Document</title>
 </head>
 <body>
-
+    Say Hi function <br>
     <form action="functions.php" method="get">
         <input type="text" name="name">
         <input type="submit">
     </form>
-
+    
     <?php
        function sayHi($name) {
         echo "Hello $name";
-    }
+        }
 
-    if (isset($_GET["name"])) {
-        $name = $_GET["name"];
-        sayHi($name);
-    }
+        if (isset($_GET["name"])) {
+            $name = $_GET["name"];
+            sayHi($name);
+        }
+    ?>
 
+    <hr>
+    Function with Return Statement: cube a number <br>
+     <form action="functions.php" method="get">
+        <input type="text" name="number">
+        <input type="submit">
+    </form>
+
+    <?php
+        function cube($num) {
+            return $num*$num*$num;
+        }
+
+        if (isset($_GET["number"])) {
+        $number= $_GET["number"];
+        echo cube($number);
+        }
     ?>
 
 </body>
