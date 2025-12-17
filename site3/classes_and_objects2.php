70271a9aeff774628f9518e862dad09e5cebd2df
Augusto <augusto170599@gmail.com>
2025-12-16 22:15:14 -0300

    concepts: 1) setter and getter functions 2) inheritance

--- site3/classes_and_objects2.php
+++ site3/classes_and_objects2.php
@@ -50,6 +50,80 @@
         
         echo ("The student $student1->name ");
         echo $student1->hasHonors();
+        echo "<br>";
+
+        class Movie {
+            public $title;
+            private $rating;
+
+            function __construct($title, $rating) {
+                $this->title = $title;
+                $this->setRating($rating);
+            }
+
+            // setter and getter function
+
+            function getRating() {
+                return $this->rating;
+            }
+
+            function setRating($rating) {
+                if($rating == "G" || $rating == "PG" || $rating == "PG-13" || $rating == "R" || $rating == "NR") {
+                    $this->rating = $rating;
+                } else {
+                    $this->rating = "NR <br>";
+                }
+            }
+        }
+
+        $avengers = new Movie("Avengers", "PG-13");
+        // can't do the following because 
+        // it's a private variable: 
+        // $avengers->rating = "Dog";
+        // echo $avengers->rating;
+        $avengers->setRating("Dog");
+        
+        echo $avengers->getRating();
+        
+
+
+        // Inheritance 
+
+        class Chef {
+
+            
+            function makeChicken() {
+                echo "The chef makes chicken <br>";
+            }
+
+            function makeSalad() {
+                echo "The chef makes a salad <br>";
+            }
+
+            function makeSpecialDish() {
+                echo "The chef makes a sandwich <br>";
+            }
+
+            
+        }
+
+        class ItalianChef extends Chef {
+            function makePasta() {
+                echo "The chef makes pasta <br>";
+            }
+
+            function makeSpecialDish() {
+                echo "The chef makes chicken parm";
+            }
+        }
+
+        $chef = new Chef();
+        $chef->makeChicken();
+        $italianChef = new ItalianChef();
+        $italianChef->makePasta();
+
+        $chef->makeSpecialDish();
+        $italianChef->makeSpecialDish();
     ?>
 </body>
 </html>
