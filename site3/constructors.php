dd49bc8fe4423c89683416a46410579898fa1990
Augusto <augusto170599@gmail.com>
2025-12-16 21:40:04 -0300

    new object with built in function

--- site3/constructors.php
+++ site3/constructors.php
@@ -23,7 +23,33 @@
         $book1->title = "Hunger Games";
         $book2 = new Book("Lord of the Rings", "Tolkien", 700);
 
-        echo $book1->title;
+        echo("$book1->title <br>");
+
+        // object function
+        class Student {
+            var $name;
+            var $major;
+            var $gpa;
+            
+            function __construct($name, $major, $gpa) {
+                $this->name = $name;
+                $this->major = $major;
+                $this->gpa = $gpa;
+            }
+
+            function hasHonors() {
+                if($this->gpa > 3.5) {
+                    return "has Honors";
+                }
+                return "does not have Honors";
+            }
+        }
+
+        $student1 = new Student("Jim", "Business", 2.8);
+        $student2 = new Student("Pam", "Art", 3.6);
+        
+        echo ("The student $student1->name ");
+        echo $student1->hasHonors();
     ?>
 </body>
 </html>
